d53a83395e260218d9048d2015d00b9ee7610017
maulru <maurojr_@outlook.com>
2021-01-19 21:09:22 -0300

    criando classe

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const cliente1Nome = "Mauro";
-const cliente1CPF = 13246204020;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente {
+  nome;
+  cpf;
+  agencia;
+  saldo;
+}
 
-const cliente2Nome = "Luana";
-const cliente2CPF = 51382154250;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
-console.log(cliente1Nome);
+cliente1.nome = "Mauro";
+cliente1.cpf = 13246204020;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = "Luana";
+cliente2.cpf = 51382154250;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1,cliente2);
