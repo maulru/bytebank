0e2d6fd58c5517de99f3e82ef79b9adfe7dd1369
maulru <maurojr_@outlook.com>
2021-01-19 21:01:16 -0300

    inicializando projeto

---
+++ index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = "Mauro";
+const cliente1CPF = 13246204020;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Luana";
+const cliente2CPF = 51382154250;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
